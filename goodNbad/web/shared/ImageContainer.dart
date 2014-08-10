e81b9e0dd519d86f8042f2436125a654bc5809fb
amine <amine@amine-PC>
2014-08-10 16:03:11 +0200

    qsfsqf

---
+++ goodNbad/web/shared/ImageContainer.dart
@@ -0,0 +1,17 @@
+part of goodNbad;
+
+class ImageContainer {
+
+	int _x;
+    int _y;
+    String _imagePath;
+
+    ImageContainer(_imagePath, _x, _y){
+
+    }
+
+	draw(CanvasRenderingContext2D context){
+
+	}
+
+}
